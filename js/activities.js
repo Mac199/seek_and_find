f5cc628c07d45e5b8efd0def92e596b56dd7c90c
Mike  Lin <mlin36@its.jnj.com>
2018-02-07 17:58:16 -0500

    adding incorrect pi image half way there

--- js/activities.js
+++ js/activities.js
@@ -2,81 +2,81 @@ var activityJSON = {
      question1: {
       question: "Dr. James would like an electronic publication to reference in order to better understand how Nucala works. What information should you provide?",
       answer1: "mirra",
-      answer2: ["mirra1", "TP1"]
+      answer2: ["m1_tp1_c"]
      },
 
      question2: {
       question: "Dr. Khan has a patient with EGPA who will always utilize the same injection site on their thigh. What information would be important for Dr. Khan to share with this patient?",
       answer1: "pi",
-      answer2: ["pi2", "TP2"]
+      answer2: ["pi2_tp2_c"]
      },
 
      question3: {
       question: "In 2012, the Chapel Hill Consensus Conference on the Nomenclature of Systemic Vasculitis revised the nomenclature and definition of vasculitides. Formerly known as Churg-Strauss syndrome, eosinophilic granulomatosis with polyangiitis (EGPA) is characterized by what?",
       answer1: "mirra",
-      answer2: ["mirra2", "TP1"]
+      answer2: ["m2_tp1_c"]
      },
 
      question4: {
       question: "A patient called Dr. Walley asking if she can freeze and reuse the remaining portion of her  reconstituted dose of Nucala. Where could Dr. Walley quickly find the answer?",
       answer1: "pi",
-      answer2: ["pi3", "TP1"]
+      answer2: ["pi3_tp1_c"]
      },
 
      question5: {
       question: "Dr. Williams is interested to learn about an assessment tool used for EGPA. Where can she find this information?",
       answer1: "mirra",
-      answer2: ["mirra10","TP2"]
+      answer2: ["m10_tp2_c"]
      },
 
      question6: {
       question: " In patients that may be prescribed Nucala, are there any opportunistic infections that physicians could vaccinate against?",
       answer1: "pi",
-      answer2: ["pi4","TP1"]
+      answer2: ["pi4_tp1_c"]
      },
      question7: {
       question: "Dr. Zhao would like to better understand drug interaction trial results for Nucala. Where can he find this information?",
       answer1: "pi",
-      answer2: ["pi8","TP1"]
+      answer2: ["pi8_tp1_c"]
      },
      question8: {
       question: "Where can a physician find information regarding the risk of pregnancy exposure to monoclonal antibodies, such as mepolizumab?",
       answer1: "pi",
-      answer2: ["pi8","TP2"]
+      answer2: ["pi8_tp2_c"]
      },
      question9: {
       question: "Dr. Allen has a patient that has been diagnosed with EGPA along with neuropathy and cardiomyopathy. Where can he look to see if these diagnoses were part of inclusions criteria in clinical trials for Nucala? ",
       answer1: "mirra",
-      answer2: ["mirra3","TP1"]
+      answer2: ["m3_tp1_c"]
      },
      question10: {
       question: "Dr. Williams wants to know if a dose higher than 300 mg of mepolizumab SC every 4 weeks could provide additional pharmacological benefit. Where could she look for an answer?",
       answer1: "mirra",
-      answer2: ["mirra9","TP1"]
+      answer2: ["m9_tp1_c"]
      },
      question11: {
       question: "If a patient calls in with questions about treatment for overdose with mepolizumab, where could they or their physician look to find information?",
       answer1: "pi",
-      answer2: ["pi10","TP1"]
+      answer2: ["pi10_tp1_c"]
      },
      question12: {
       question: "Where would you find a table containing the most commonly reported adverse events for subjects with EGPA?",
       answer1: "mirra",
-      answer2: ["mirra10","TP1"]
+      answer2: ["m10_tp1_c"]
      },
      question13: {
       question: "Where would you find a table containing the most commonly reported adverse events for subjects with severe asthma?",
       answer1: "pi",
-      answer2: ["pi5","TP1"]
+      answer2: ["pi5_tp1_c"]
      },
      question14: {
       question: "Where could a physician find demographics and baseline characteristics for subjects across all three clinical trials for severe asthma?",
       answer1: "pi",
-      answer2: ["pi14","TP1"]
+      answer2: ["pi14_tp1_c"]
      },
      question15: {
       question: "Dr. Jones would like to know more about the effect of mepolizumab on lung function. Where should you instruct him to look?",
       answer1: "pi",
-      answer2: ["pi17","TP1"]
+      answer2: ["pi17_tp1_c"]
      }
 }
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-console.log(activityJSON);
+  var $footer = $('footer');
   var number = 1;
   var $resource;
   $laucn = $("#laucn");
@@ -18,91 +18,243 @@ console.log(activityJSON);
    //**************** CHOOSE THE CORRECT RESOURCE *****************//
   $('body').on('click','.resource',function(){    
     $resource = $(this).attr("alt");  
-    $('footer').css('display','inherit');
+    $footer.css('display','inherit');
     if($resource == activityJSON['question'+number]["answer1"]){
       
       if($("#next_question").length ==0 ){
-        $("footer").html("<img src='images/Screens/correct_resource.png'><button id='next_question'>NEXT QUESTION</button>");
-      }else{$('footer').css('display','inherit')};
+        $footer.html("<img src='images/Screens/correct_resource.png'><button id='next_question'>NEXT QUESTION</button>");
+      }else{$footer.css('display','inherit')};
       
       
       $('#next_question').click(function(){
         $(".resource").css('display', 'none');
-        $("footer").css('display', 'none');
+        $footer.css('display', 'none');
         
         if($resource == "mirra"){
-           console.log("this is mirra");
-          $("#resource").append("<img src='images/mirra_pages/mirra_page_1.jpg' alt='mirra1'>"+
-              "<img src='images/mirra_pages/mirra_page_2.jpg' alt='mirra2'>"+
-              "<img src='images/mirra_pages/mirra_page_3.jpg' alt='mirra3'>"+
-              "<img src='images/mirra_pages/mirra_page_4.jpg' alt='mirra4'>"+
-              "<img src='images/mirra_pages/mirra_page_5.jpg' alt='mirra5'>"+
-              "<img src='images/mirra_pages/mirra_page_6.jpg' alt='mirra6'>"+
-              "<img src='images/mirra_pages/mirra_page_7.jpg' alt='mirra7'>"+
-              "<img src='images/mirra_pages/mirra_page_8.jpg' alt='mirra8'>"+
-              "<img src='images/mirra_pages/mirra_page_9.jpg' alt='mirra9'>"+
-              "<img src='images/mirra_pages/mirra_page_10.jpg' alt='mirra10'>"+
-              "<img src='images/mirra_pages/mirra_page_11.jpg' alt='mirra11'>"+
-              "<img src='images/mirra_pages/mirra_page_12.jpg' alt='mirra12'>");
-          choose_tp();
+          $("#resource").append("<div class='page'><img src='images/mirra_pages/mirra_page_1.jpg' alt='mirra1'>"+
+              //********* first page
+              "<img src='images/mirra_pages/M1_TP1_c.png' alt='m1_tp1_c' class='answer'>"+"<img src='images/mirra_pages/M1_TP1_incorrect.png' alt='m1_tp1_inc' class='answer'>"+
+              
+              "<img src='images/mirra_pages/M1_TP2_c.png' alt='m1_tp2_c' class='answer'>"+"<img src='images/mirra_pages/M1_TP2_incorrect.png' alt='m1_tp2_inc' class='answer'>"+
+              
+              "<img src='images/mirra_pages/M1_TP3_c.png' alt='m1_tp3_c' class='answer'><img src='images/mirra_pages/M1_TP3_incorrect.png' alt='m1_tp3_inc' class='answer'></div>"+
+              
+              //************ seconde page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_2.jpg' alt='mirra2'>"+
+              
+              "<img src='images/mirra_pages/M2_TP1_c.png' alt='m2_tp1_c' class='answer'>"+"<img src='images/mirra_pages/M2_TP1_incorrect.png' alt='m2_tp1_inc' class='answer'>"+
+              
+              "<img src='images/mirra_pages/M2_TP2_c.png' alt='m2_tp2_c' class='answer'>"+"<img src='images/mirra_pages/M2_TP2_incorrect.png' alt='m2_tp2_inc' class='answer'>"+
+
+              "<img src='images/mirra_pages/M2_TP3_c.png' alt='m2_tp3_c' class='answer'><img src='images/mirra_pages/M2_TP3_incorrect.png' alt='m2_tp3_inc' class='answer'></div>"+
+              
+              
+              //*********** third page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_3.jpg' alt='mirra3'>"+
+              "<img src='images/mirra_pages/M3_TP1_c.png' alt='m3_tp1_c' class='answer'><img src='images/mirra_pages/M3_TP1_incorrect.png' alt='m3_tp1_inc' class='answer'>"+
+              "</div>"+
+              
+              
+              //************ fouth page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_4.jpg' alt='mirra4'></div>"+
+              
+              
+              //************ 5 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_5.jpg' alt='mirra5'>"+
+              "<img src='images/mirra_pages/M5_TP1_c.png' alt='m5_tp1_c' class='answer'>"+"<img src='images/mirra_pages/M5_TP1_incorrect.png' alt='m5_tp1_inc' class='answer'>"+
+              "</div>"+
+              
+              
+
+              //************ 6 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_6.jpg' alt='mirra6'>"+
+              "<img src='images/mirra_pages/M6_TP1_c.png' alt='m6_tp1_c' class='answer'><img src='images/mirra_pages/M6_TP1_incorrect.png' alt='m6_tp1_inc' class='answer'></div>"+
+              
+              
+              //************ 7 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_7.jpg' alt='mirra7'></div>"+
+              
+              
+              //************ 8 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_8.jpg' alt='mirra8'>"+
+              "<img src='images/mirra_pages/M8_TP1_c.png' alt='m8_tp1_c' class='answer'>"+"<img src='images/mirra_pages/M8_TP1_incorrect.png' alt='m8_tp1_inc' class='answer'>"+
+              "<img src='images/mirra_pages/M8_TP2_c.png' alt='m8_tp2_c' class='answer'>"+"<img src='images/mirra_pages/M8_TP2_incorrect.png' alt='m8_tp2_inc' class='answer'>"+
+              "<img src='images/mirra_pages/M8_TP3_c.png' alt='m8_tp3_c' class='answer'>"+"<img src='images/mirra_pages/M8_TP3_incorrect.png' alt='m8_tp3_inc' class='answer'>"
+              +"<img src='images/mirra_pages/M8_TP4_c.png' alt='m8_tp4_c' class='answer'>"+"<img src='images/mirra_pages/M8_TP4_incorrect.png' alt='m8_tp4_inc' class='answer'>"+
+              "</div>"+
+              
+              
+              //************ 9 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_9.jpg' alt='mirra9'>"+
+              "<img src='images/mirra_pages/M9_TP1_c.png' alt='m9_tp1_c' class='answer'>"+"<img src='images/mirra_pages/M9_TP1_incorrect.png' alt='m9_tp1_inc' class='answer'>"+
+              "</div>"+
+              
+              
+              //************ 10 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_10.jpg' alt='mirra10'>"+
+              "<img src='images/mirra_pages/M10_TP1_c.png' alt='m10_tp1_c' class='answer'>"+"<img src='images/mirra_pages/M10_TP1_incorrect.png' alt='m10_tp1_inc' class='answer'>"+
+              "<img src='images/mirra_pages/M10_TP2_c.png' alt='m10_tp2_c' class='answer'>"+"<img src='images/mirra_pages/M10_TP2_incorrect.png' alt='m10_tp2_inc' class='answer'>"+
+              "</div>"+
+              
+              
+              //************ 11 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_11.jpg' alt='mirra11'></div>"+
+              
+              
+              //************ 12 page
+              "<div class='page'><img src='images/mirra_pages/mirra_page_12.jpg' alt='mirra12'></div>");
+              check_tp_result();
         }
         
         else if($resource == "pi"){
-          console.log("this is pi");
-              $("#resource").append("<img src='images/pi/nucala_pi_01.jpg' alt='pi1' >"+
-                              "<img src='images/pi/nucala_pi_02.jpg' alt='pi2'>"+
-                              "<img src='images/pi/nucala_pi_03.jpg' alt='pi3'>"+
-                              "<img src='images/pi/nucala_pi_04.jpg' alt='pi4'>"+
-                              "<img src='images/pi/nucala_pi_05.jpg' alt='pi5'>"+
-                              "<img src='images/pi/nucala_pi_06.jpg' alt='pi6'>"+
-                              "<img src='images/pi/nucala_pi_07.jpg' alt='pi7'>"+
-                              "<img src='images/pi/nucala_pi_08.jpg' alt='pi8'>"+
-                              "<img src='images/pi/nucala_pi_09.jpg' alt='pi9'>"+
-                              "<img src='images/pi/nucala_pi_10.jpg' alt='pi10'>"+
-                              "<img src='images/pi/nucala_pi_11.jpg' alt='pi11'>"+
-                              "<img src='images/pi/nucala_pi_12.jpg' alt='pi12'>"+
-                              "<img src='images/pi/nucala_pi_13.jpg' alt='pi13'>"+
-                              "<img src='images/pi/nucala_pi_14.jpg' alt='pi14'>"+
-                              "<img src='images/pi/nucala_pi_15.jpg' alt='pi15'>"+
-                              "<img src='images/pi/nucala_pi_16.jpg' alt='pi16'>"+
-                              "<img src='images/pi/nucala_pi_17.jpg' alt='pi17'>"+
-                              "<img src='images/pi/nucala_pi_18.jpg' alt='pi18'>"+
-                              "<img src='images/pi/nucala_pi_19.jpg' alt='pi19'>"+
-                              "<img src='images/pi/nucala_pi_20.jpg' alt='pi20'>"+
-                              "<img src='images/pi/nucala_pi_21.jpg' alt='pi21'>"+
-                              "<img src='images/pi/nucala_pi_22.jpg' alt='pi22'>"+
-                              "<img src='images/pi/nucala_pi_23.jpg' alt='pi23'>"+
-                              "<img src='images/pi/nucala_pi_24.jpg' alt='pi24'>");
-              choose_tp();
+              $("#resource").append("<div class='page'><img src='images/pi/nucala_pi_01.jpg' alt='pi1' ></div>"+
+
+                              //********* 2 page
+                              "<div class='page'><img src='images/pi/nucala_pi_02.jpg' alt='pi2'>"+
+                              "<img src='images/pi/P2_TP1_c.png' alt='pi2_tp1_c'>"+"<img src='images/pi/P2_TP1_incorrect.png' alt='pi2_tp1_inc'>"+
+                              "<img src='images/pi/P2_TP2_c.png' alt='pi2_tp2_c'>"+"<img src='images/pi/P2_TP2_incorrect.png' alt='pi2_tp2_inc'>"+
+                              "<img src='images/pi/P2_TP3_c.png' alt='pi2_tp3_c'>"+"<img src='images/pi/P2_TP3_c.png' alt='pi2_tp3_inc'>"+
+                              "</div>"+
+                              
+                              //********* 3 page
+                              "<div class='page'><img src='images/pi/nucala_pi_03.jpg' alt='pi3'>"+
+                              "<img src='images/pi/P3_TP1_c.png' alt='pi3_tp1_c'>"+"<img src='images/pi/P3_TP1_incorrect.png' alt='pi3_tp1_inc'>"+
+                              "<img src='images/pi/P3_TP2_c.png' alt='pi3_tp2_c'>"+"<img src='images/pi/P3_TP2_incorrect.png' alt='pi3_tp2_inc'>"+
+                              "</div>"+
+                              
+                              //********* 4 page
+                              "<div class='page'><img src='images/pi/nucala_pi_04.jpg' alt='pi4'>"+
+                              "<img src='images/pi/P4_TP1_c.png' alt='pi4_tp1_c'>"+"<img src='images/pi/P4_TP1_incorrect.png' alt='pi4_tp1_inc'>"+
+                              "<img src='images/pi/P4_TP2_c.png' alt='pi4_tp2_c'>"+"<img src='images/pi/P4_TP2_incorrect.png' alt='pi4_tp2_inc'>"+
+                              "</div>"+
+                              
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_05.jpg' alt='pi5'>"+
+                              "<img src='images/pi/P5_TP1_c.png' alt='pi5_tp1_c'>"+"<img src='images/pi/P5_TP1_incorrect.png' alt='pi5_tp1_inc'>"+
+                              "</div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_06.jpg' alt='pi6'>"+
+                              "<img src='images/pi/P6_TP1_c.png' alt='pi6_tp1_c'>"+"<img src='images/pi/P6_TP1_incorrect.png' alt='pi6_tp1_inc'>"+
+                              "</div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_07.jpg' alt='pi7'><img src='images/pi/P7_TP1_c.png' alt='pi7_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_08.jpg' alt='pi8'><img src='images/pi/P8_TP1_c.png' alt='pi8_tp1_c'><img src='images/pi/P8_TP2_c.png' alt='pi8_tp2_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_09.jpg' alt='pi9'><img src='images/pi/P9_TP1_c.png' alt='pi9_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_10.jpg' alt='pi10'><img src='images/pi/P10_TP1_c.png' alt='pi10_tp1_c'><img src='images/pi/P10_TP2_c.png' alt='pi10_tp2_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_11.jpg' alt='pi11'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_12.jpg' alt='pi12'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_13.jpg' alt='pi13'><img src='images/pi/P13_TP1_c.png' alt='pi13_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_14.jpg' alt='pi14'><img src='images/pi/P14_TP1_c.png' alt='pi14_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_15.jpg' alt='pi15'><img src='images/pi/P15_TP1_c.png' alt='pi15_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_16.jpg' alt='pi16'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_17.jpg' alt='pi17'><img src='images/pi/P17_TP1_c.png' alt='pi17_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_18.jpg' alt='pi18'><img src='images/pi/P18_TP1_c.png' alt='pi18_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_19.jpg' alt='pi19'><img src='images/pi/P19_TP1_c.png' alt='pi19_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_20.jpg' alt='pi20'><img src='images/pi/P20_TP1_c.png' alt='pi20_tp1_c'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_21.jpg' alt='pi21'><<img src='images/pi/P21_TP1_c.png' alt='pi21_tp1_c'>/div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_22.jpg' alt='pi22'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_23.jpg' alt='pi23'></div>"+
+                              
+
+                              //*********
+                              "<div class='page'><img src='images/pi/nucala_pi_24.jpg' alt='pi24'></div>");
+              check_tp_result();
             }
         })
     }
         else{
-          $('footer').html("<img src='images/Screens/incorrect_resource.png'>");
+          $footer.html("<img src='images/Screens/incorrect_resource.png'>");
         } 
   });
   //**************** CHOOSE THE CORRECT RESOURCE ENDS*****************//
-
-  function choose_tp() {
-    if('right touch point'){
-      $("footer").css('display', 'inherit');
-      $("#next_question").remove();
-      $("footer").append("<img src='images/Buttons/load_next_scenario.png' class = 'load_next_scenario'>")
-    }
-    else{
-      alert("keep looping");
-    }
-  }
 
   $("body").on("click", ".load_next_scenario", function() {  
        number = number+1;
        console.log(number);
        $("#questions").html(activityJSON['question'+number]["question"]);
-       $("footer").css('display', 'none');
-       $("#resource img").remove();
+       $footer.css('display', 'none');
+       $("#resource .page").remove();
        $(".resource").css('display','inherit');
        if(number == 16) {
         $app.css('background-image',"url('/images/congrats.png')")
        }
   })
+
+
+  function check_tp_result(){
+     $('#resource').on('click','.answer',function(){    
+     var result = $(this).attr('alt');
+     if(result == activityJSON['question'+number]["answer2"]){
+        $footer.css('display', 'inherit');
+        $("#next_question").remove();
+        $footer.html("<img src='images/Screens/correct_datapoint.png'><img src='images/Buttons/load_next_scenario.png' class = 'load_next_scenario'>");
+     }
+     else{
+       $footer.html("<img src='images/Screens/incorrect_datapoint.png' class='incorrect_datapoint'>");
+       $footer.css('display','inherit');
+       $(this).css('display','none');
+       $(this).next().css('display','inherit');
+     }
+    }) 
+  }
+
+
+
+
 });// ********** Document ready ends ***********//
+
+
+
+
