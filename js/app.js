11380f1ddefaa56fb0422eb63cc5d560d6b55e1e
Mike  Lin <mlin36@its.jnj.com>
2018-02-08 13:52:24 -0500

    timer and score

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   var $footer = $('footer');
   var number = 1;
   var $resource;
+  var points = 0;
   $laucn = $("#laucn");
   $app = $("#app");
-
+  $point = $("#point");
   $laucn.click(function(){ 
     $("#app").css('background-image',"url('images/select_resource.png')");
     $(".grow").remove();
@@ -20,7 +21,8 @@ $(document).ready(function(){
     $resource = $(this).attr("alt");  
     $footer.css('display','inherit');
     if($resource == activityJSON['question'+number]["answer1"]){
-      
+      points = points + 50;
+      $point.html(points);
       if($("#next_question").length ==0 ){
         $footer.html("<img src='images/Screens/correct_resource.png'><button id='next_question'>NEXT QUESTION</button>");
       }else{$footer.css('display','inherit')};
@@ -103,7 +105,6 @@ $(document).ready(function(){
               
               //************ 12 page
               "<div class='page'><img src='images/mirra_pages/mirra_page_12.jpg' alt='mirra12'></div>");
-              check_tp_result();
         }
         
         else if($resource == "pi"){
@@ -111,106 +112,131 @@ $(document).ready(function(){
 
                               //********* 2 page
                               "<div class='page'><img src='images/pi/nucala_pi_02.jpg' alt='pi2'>"+
-                              "<img src='images/pi/P2_TP1_c.png' alt='pi2_tp1_c'>"+"<img src='images/pi/P2_TP1_incorrect.png' alt='pi2_tp1_inc'>"+
-                              "<img src='images/pi/P2_TP2_c.png' alt='pi2_tp2_c'>"+"<img src='images/pi/P2_TP2_incorrect.png' alt='pi2_tp2_inc'>"+
-                              "<img src='images/pi/P2_TP3_c.png' alt='pi2_tp3_c'>"+"<img src='images/pi/P2_TP3_c.png' alt='pi2_tp3_inc'>"+
+                              "<img src='images/pi/P2_TP1_c.png' alt='pi2_tp1_c' class='answer'>"+"<img src='images/pi/P2_TP1_incorrect.png' alt='pi2_tp1_inc' class='answer'>"+
+                              "<img src='images/pi/P2_TP2_c.png' alt='pi2_tp2_c' class='answer'>"+"<img src='images/pi/P2_TP2_incorrect.png' alt='pi2_tp2_inc' class='answer'>"+
+                              "<img src='images/pi/P2_TP3_c.png' alt='pi2_tp3_c' class='answer'>"+"<img src='images/pi/P2_TP3_incorrect.png' alt='pi2_tp3_inc' class='answer'>"+
                               "</div>"+
                               
                               //********* 3 page
                               "<div class='page'><img src='images/pi/nucala_pi_03.jpg' alt='pi3'>"+
-                              "<img src='images/pi/P3_TP1_c.png' alt='pi3_tp1_c'>"+"<img src='images/pi/P3_TP1_incorrect.png' alt='pi3_tp1_inc'>"+
-                              "<img src='images/pi/P3_TP2_c.png' alt='pi3_tp2_c'>"+"<img src='images/pi/P3_TP2_incorrect.png' alt='pi3_tp2_inc'>"+
+                              "<img src='images/pi/P3_TP1_c.png' alt='pi3_tp1_c' class='answer'>"+"<img src='images/pi/P3_TP1_incorrect.png' alt='pi3_tp1_inc' class='answer' class='answer'>"+
+                              "<img src='images/pi/P3_TP2_c.png' alt='pi3_tp2_c' class='answer'>"+"<img src='images/pi/P3_TP2_incorrect.png' alt='pi3_tp2_inc' class='answer' class='answer'>"+
                               "</div>"+
                               
                               //********* 4 page
                               "<div class='page'><img src='images/pi/nucala_pi_04.jpg' alt='pi4'>"+
-                              "<img src='images/pi/P4_TP1_c.png' alt='pi4_tp1_c'>"+"<img src='images/pi/P4_TP1_incorrect.png' alt='pi4_tp1_inc'>"+
-                              "<img src='images/pi/P4_TP2_c.png' alt='pi4_tp2_c'>"+"<img src='images/pi/P4_TP2_incorrect.png' alt='pi4_tp2_inc'>"+
+                              "<img src='images/pi/P4_TP1_c.png' alt='pi4_tp1_c' class='answer'>"+"<img src='images/pi/P4_TP1_incorrect.png' alt='pi4_tp1_inc' class='answer'>"+
+                              "<img src='images/pi/P4_TP2_c.png' alt='pi4_tp2_c' class='answer'>"+"<img src='images/pi/P4_TP2_incorrect.png' alt='pi4_tp2_inc' class='answer'>"+
                               "</div>"+
                               
-                              //*********
+                              //********* 5
                               "<div class='page'><img src='images/pi/nucala_pi_05.jpg' alt='pi5'>"+
-                              "<img src='images/pi/P5_TP1_c.png' alt='pi5_tp1_c'>"+"<img src='images/pi/P5_TP1_incorrect.png' alt='pi5_tp1_inc'>"+
+                              "<img src='images/pi/P5_TP1_c.png' alt='pi5_tp1_c' class='answer'>"+"<img src='images/pi/P5_TP1_incorrect.png' alt='pi5_tp1_inc' class='answer'>"+
                               "</div>"+
                               
 
-                              //*********
+                              //********* 6
                               "<div class='page'><img src='images/pi/nucala_pi_06.jpg' alt='pi6'>"+
-                              "<img src='images/pi/P6_TP1_c.png' alt='pi6_tp1_c'>"+"<img src='images/pi/P6_TP1_incorrect.png' alt='pi6_tp1_inc'>"+
+                              "<img src='images/pi/P6_TP1_c.png' alt='pi6_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P6_TP1_incorrect.png' alt='pi6_tp1_inc' class='answer'>"+
                               "</div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_07.jpg' alt='pi7'><img src='images/pi/P7_TP1_c.png' alt='pi7_tp1_c'></div>"+
+                              //********* 7
+                              "<div class='page'><img src='images/pi/nucala_pi_07.jpg' alt='pi7'>"+
+                              "<img src='images/pi/P7_TP1_c.png' alt='pi7_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P7_TP1_incorrect.png' alt='pi7_tp1_inc' class='answer'>"+
+                              "</div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_08.jpg' alt='pi8'><img src='images/pi/P8_TP1_c.png' alt='pi8_tp1_c'><img src='images/pi/P8_TP2_c.png' alt='pi8_tp2_c'></div>"+
+                              //********* 8
+                              "<div class='page'><img src='images/pi/nucala_pi_08.jpg' alt='pi8'>"+
+                              "<img src='images/pi/P8_TP1_c.png' alt='pi8_tp1_c' class='answer'>"+"<img src='images/pi/P8_TP1_incorrect.png' alt='pi8_tp1_inc' class='answer'>"+
+                              "<img src='images/pi/P8_TP2_c.png' alt='pi8_tp2_c' class='answer'><img src='images/pi/P8_TP2_incorrect.png' alt='pi8_tp2_inc' class='answer'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_09.jpg' alt='pi9'><img src='images/pi/P9_TP1_c.png' alt='pi9_tp1_c'></div>"+
+                              //********* 9
+                              "<div class='page'><img src='images/pi/nucala_pi_09.jpg' alt='pi9'>"+
+                              "<img src='images/pi/P9_TP1_c.png' alt='pi9_tp1_c' class='answer'><img src='images/pi/P9_TP1_incorrect.png' alt='pi9_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_10.jpg' alt='pi10'><img src='images/pi/P10_TP1_c.png' alt='pi10_tp1_c'><img src='images/pi/P10_TP2_c.png' alt='pi10_tp2_c'></div>"+
+                              //********* 10
+                              "<div class='page'><img src='images/pi/nucala_pi_10.jpg' alt='pi10'>"+
+                              "<img src='images/pi/P10_TP1_c.png' alt='pi10_tp1_c' class='answer'>"+"<img src='images/pi/P10_TP1_incorrect.png' alt='pi10_tp1_inc' class='answer'>"+
+                              "<img src='images/pi/P10_TP2_c.png' alt='pi10_tp2_c' class='answer'><img src='images/pi/P10_TP2_incorrect.png' alt='pi10_tp2_inc' class='answer'></div>"+
                               
 
-                              //*********
+                              //********* 11
                               "<div class='page'><img src='images/pi/nucala_pi_11.jpg' alt='pi11'></div>"+
                               
 
-                              //*********
+                              //********* 12
                               "<div class='page'><img src='images/pi/nucala_pi_12.jpg' alt='pi12'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_13.jpg' alt='pi13'><img src='images/pi/P13_TP1_c.png' alt='pi13_tp1_c'></div>"+
+                              //********* 13
+                              "<div class='page'><img src='images/pi/nucala_pi_13.jpg' alt='pi13'><img src='images/pi/P13_TP1_c.png' alt='pi13_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P13_TP1_incorrect.png' alt='pi13_tp1_inc' class='answer'>"+
+                              +"</div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_14.jpg' alt='pi14'><img src='images/pi/P14_TP1_c.png' alt='pi14_tp1_c'></div>"+
+                              //********* 14
+                              "<div class='page'><img src='images/pi/nucala_pi_14.jpg' alt='pi14'>"+
+                              "<img src='images/pi/P14_TP1_c.png' alt='pi14_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P14_TP1_incorrect.png' alt='pi14_tp1_inc' class='answer'>"+
+                              "</div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_15.jpg' alt='pi15'><img src='images/pi/P15_TP1_c.png' alt='pi15_tp1_c'></div>"+
+                              //********* 15
+                              "<div class='page'><img src='images/pi/nucala_pi_15.jpg' alt='pi15'>"+
+                              "<img src='images/pi/P15_TP1_c.png' alt='pi15_tp1_c' class='answer'><img src='images/pi/P15_TP1_incorrect.png' alt='pi15_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
+                              //********* 16
                               "<div class='page'><img src='images/pi/nucala_pi_16.jpg' alt='pi16'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_17.jpg' alt='pi17'><img src='images/pi/P17_TP1_c.png' alt='pi17_tp1_c'></div>"+
+                              //********* 17
+                              "<div class='page'><img src='images/pi/nucala_pi_17.jpg' alt='pi17'>"+
+                              "<img src='images/pi/P17_TP1_c.png' alt='pi17_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P17_TP1_incorrect.png' alt='pi17_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_18.jpg' alt='pi18'><img src='images/pi/P18_TP1_c.png' alt='pi18_tp1_c'></div>"+
+                              //********* 18
+                              "<div class='page'><img src='images/pi/nucala_pi_18.jpg' alt='pi18'>"+
+                              "<img src='images/pi/P18_TP1_c.png' alt='pi18_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P18_TP1_incorrect.png' alt='pi18_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_19.jpg' alt='pi19'><img src='images/pi/P19_TP1_c.png' alt='pi19_tp1_c'></div>"+
+                              //********* 19
+                              "<div class='page'><img src='images/pi/nucala_pi_19.jpg' alt='pi19'>"+
+                              "<img src='images/pi/P19_TP1_c.png' alt='pi19_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P19_TP1_incorrect.png' alt='pi19_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_20.jpg' alt='pi20'><img src='images/pi/P20_TP1_c.png' alt='pi20_tp1_c'></div>"+
+                              //********* 20
+                              "<div class='page'><img src='images/pi/nucala_pi_20.jpg' alt='pi20'>"+
+                              "<img src='images/pi/P20_TP1_c.png' alt='pi20_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P20_TP1_incorrect.png' alt='pi20_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
-                              "<div class='page'><img src='images/pi/nucala_pi_21.jpg' alt='pi21'><<img src='images/pi/P21_TP1_c.png' alt='pi21_tp1_c'>/div>"+
+                              //********* 21
+                              "<div class='page'><img src='images/pi/nucala_pi_21.jpg' alt='pi21'>"+
+                              "<img src='images/pi/P21_TP1_c.png' alt='pi21_tp1_c' class='answer'>"+
+                              "<img src='images/pi/P21_TP1_incorrect.png' alt='pi21_tp1_inc' class='answer'></div>"+
                               
 
-                              //*********
+                              //********* 22
                               "<div class='page'><img src='images/pi/nucala_pi_22.jpg' alt='pi22'></div>"+
                               
 
-                              //*********
+                              //********* 23
                               "<div class='page'><img src='images/pi/nucala_pi_23.jpg' alt='pi23'></div>"+
                               
 
-                              //*********
+                              //********* 24
                               "<div class='page'><img src='images/pi/nucala_pi_24.jpg' alt='pi24'></div>");
-              check_tp_result();
+
             }
         })
     }
@@ -222,7 +248,6 @@ $(document).ready(function(){
 
   $("body").on("click", ".load_next_scenario", function() {  
        number = number+1;
-       console.log(number);
        $("#questions").html(activityJSON['question'+number]["question"]);
        $footer.css('display', 'none');
        $("#resource .page").remove();
@@ -233,24 +258,30 @@ $(document).ready(function(){
   })
 
 
-  function check_tp_result(){
-     $('#resource').on('click','.answer',function(){    
-     var result = $(this).attr('alt');
-     if(result == activityJSON['question'+number]["answer2"]){
-        $footer.css('display', 'inherit');
-        $("#next_question").remove();
-        $footer.html("<img src='images/Screens/correct_datapoint.png'><img src='images/Buttons/load_next_scenario.png' class = 'load_next_scenario'>");
-     }
-     else{
-       $footer.html("<img src='images/Screens/incorrect_datapoint.png' class='incorrect_datapoint'>");
-       $footer.css('display','inherit');
-       $(this).css('display','none');
-       $(this).next().css('display','inherit');
-     }
-    }) 
-  }
-
-
+   $('#resource').on('click','.answer',function(){    
+   var result = $(this).attr('alt');
+   if(result == activityJSON['question'+number]["answer2"]){
+      points = points + 50;
+      $point.html(points);
+      $(this).removeClass("answer");
+      $time = $("#timer").text();
+      $footer.css('display', 'inherit');
+      $("#next_question").remove();
+      $footer.html("<img src='images/Screens/correct_datapoint.png'><img src='images/Buttons/load_next_scenario.png' class = 'load_next_scenario'>");
+   }
+   else{
+     $footer.html("<img src='images/Screens/incorrect_datapoint.png' class='incorrect_datapoint'>");
+     $footer.css('display','inherit');
+     $(this).css('display','none');
+     $(this).next().css('display','inherit');
+   }
+  }) 
+//****************** count time ************//
+    // if($time == "0:00"){
+    //   $app.css('background-image',"url('images/congrats.png')");
+    //   $('#resource .page').remove();
+    //   $("#final_point").html(points).css('display','inherit');
+    // };
 
 
 });// ********** Document ready ends ***********//
--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var timer2 = "4:00";
+var timer2 = "1:00";
 function timer() {
 var interval = setInterval(function() {
   var timer = timer2.split(':');
@@ -10,10 +10,17 @@ var interval = setInterval(function() {
   seconds = (seconds < 0) ? 59 : seconds;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
   //minutes = (minutes < 10) ?  minutes : minutes;
-  $('.countdown').html(minutes + ':' + seconds);
-  if (minutes < 0) clearInterval(interval);
+  // $('.countdown').html(minutes + ':' + seconds);
+  // if (minutes < 0) {
+  //   clearInterval(interval);
+  // };
   //check if both minutes and seconds are 0
-  if ((seconds <= 0) && (minutes <= 0)) clearInterval(interval);
+  if ((seconds <= 0) && (minutes <= 0)){
+    clearInterval(interval);
+    $("#app").css('background-image',"url('images/congrats.png')");
+    $('#resource .page').remove();
+    $("#final_point").html($("#point")).css('display','inherit');
+  };
   timer2 = minutes + ':' + seconds;
 }, 1000);
 } 
